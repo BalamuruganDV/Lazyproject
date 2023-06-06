441aeef24af2aa3e214f8beab45937eee66369de
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-06-06 16:15:33 +0530

    Update and rename rlazyRenamer.py to Renamer.py

--- plugins/Renamer.py
+++ plugins/Renamer.py
@@ -1,33 +1,3 @@
-"""
-Apache License 2.0
-Copyright (c) 2023 @LazyDeveloper
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-Dev Channel Link : https://t.me/LazyDeveloper 
-Repo Link : https://github.com/LazyDeveloperr/LazyPrincess
-License Link : https://github.com/LazyDeveloperr/LazyPrincess/blob/main/LICENSE
-# Removing this is strictly prohibited ! Don't remove this all without the 
-permission of LazyDeveloperr
-"""
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-        # Born to make history @LazyDeveloper !
-
-    # Thank you LazyDeveloper for helping us in this Journey
-
 from asyncio import sleep
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -35,9 +5,6 @@ from pyrogram.errors import FloodWait
 import humanize
 from info import ADMINS , FLOOD, LAZY_MODE, LAZY_RENAMERS
 import random
-
-
-# Born to make history @LazyDeveloper !
 
 @Client.on_message( filters.private & (filters.document | filters.audio | filters.video))
 async def rename_start(client, message):
