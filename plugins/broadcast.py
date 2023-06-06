84ba6dec3465955fed3278722991a5bcc29ff731
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-06-06 17:30:22 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,4 +1,3 @@
-
 from pyrogram import Client, filters
 import datetime
 import time
@@ -8,7 +7,6 @@ from utils import broadcast_messages
 import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/LazyDeveoper
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
