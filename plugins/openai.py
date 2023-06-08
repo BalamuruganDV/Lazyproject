c0d5d642cbf16c0fd042bb58df2eef57005a40df
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-06-08 18:08:07 +0530

    Fast as flash

--- plugins/openai.py
+++ plugins/openai.py
@@ -6,16 +6,16 @@ import openai
 openai.api_key = OPENAI_API
 
 @Client.on_message(filters.command("ask"))
-async def lazy_answer(client, message):
+async def answer(client, message):
     if AI == True: 
         user_id = message.from_user.id
         if user_id:
             try:
-                lazy_users_message = message.text
+                users_message = message.text
                 user_id = message.from_user.id
                 response = openai.Completion.create(
                     model = "text-davinci-003",
-                    prompt = lazy_users_message,
+                    prompt = users_message,
                     temperature = 0.5, 
                     max_tokens = 1000,
                     top_p=1,
@@ -27,10 +27,10 @@ async def lazy_answer(client, message):
                         [InlineKeyboardButton(text=f"🗑 Delete log ❌", callback_data=f'close_data')],
                     ]
                 reply_markup=InlineKeyboardMarkup(btn)
-                lazy_response = response.choices[0].text 
-                await client.send_message(AI_LOGS, text=f"⚡️⚡️#AI_Query \n<b> User </b> :- {message.from_user.mention}\n<b> User id </b> :- {user_id}\n\n<b> Asked me </b> \n {lazy_users_message} \n\n<b> I responded </b>{lazy_response}", 
+                response = response.choices[0].text 
+                await client.send_message(AI_LOGS, text=f"⚡️⚡️#AI_Query \n<b> User </b> :- {message.from_user.mention}\n<b> User id </b> :- {user_id}\n\n<b> Asked me </b> \n {users_message} \n\n<b> I responded </b>{response}", 
                 reply_markup = reply_markup )
-                await message.reply(f"{lazy_response}")
+                await message.reply(f"{response}")
             except Exception as error:
                 print(error)
                 await message.reply_text(f'Error occured\n\n{error}')
