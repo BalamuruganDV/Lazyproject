c493511df6363316ce672fe6a2ca5551547d117f
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-06-08 14:40:53 +0530

    Update password_gen.py

--- plugins/Modules/password_gen.py
+++ plugins/Modules/password_gen.py
@@ -15,5 +15,4 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/mkn_bots_updates')]])
-    await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
+    await message.edit_text(text=txt)
