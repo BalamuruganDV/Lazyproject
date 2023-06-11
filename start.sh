a996fc5d585c35c814f24744f3486b8568374090
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2023-06-11 16:25:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BalamuruganDV/Uchiha-itachi /Uchiha-itachi
+  git clone https://github.com/BalamuruganDV/Uchiha-itachi.git /Uchiha-itachi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Uchiha-itachi
 fi
 cd /Uchiha-itachi
 pip3 install -U -r requirements.txt
-echo "Starting Uchiha❟❛❟...."
+echo "𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝚄𝚌𝚑𝚒𝚑𝚊..."
 python3 bot.py
